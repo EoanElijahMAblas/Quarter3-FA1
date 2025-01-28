2803709c9f7e5f019a9f5e5c18d9597d6d76c21d
Eoan Elijah M. Ablas <eoanelijah.ablas@smc.pshs.edu.ph>
2025-01-28 16:41:45 +0800

    Update prob2.js
    fixed some parts

--- prob2.js
+++ prob2.js
@@ -3,5 +3,5 @@ function prob2() {
     var lname = prompt("Enter Last Name: ");
     var year = prompt("Enter Birth Year: ");
     var age = 2025-year;
-    document.write("Hi " + fname + " " + lname + "! How does it feel to be " + age + " years old?" );
+    document.getElementById("prob2").innerHTML = "Hi " + fname + " " + lname + "! How does it feel to be " + age + " years old?" ;
 }
